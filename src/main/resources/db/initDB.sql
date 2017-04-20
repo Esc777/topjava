1dbac7f9eb9a8f6786a01f2db9803c4371c35a6f
Esc777 <Uni_Symieb@mail.ru>
2017-04-20 15:49:06 +0800

    4

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -34,14 +34,3 @@ CREATE TABLE meals (
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 CREATE UNIQUE INDEX meals_unique_user_datetime_idx ON meals(user_id, date_time)
-
-CREATE TABLE meals
-(
-  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  user_id INTEGER NOT NULL,
-  description VARCHAR NOT NULL,
-  calories INT NOT NULL,
-  date_time TIMESTAMP NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
-CREATE UNIQUE INDEX meals_unique_user_datetime_idx ON meals(user_id,date_time)
