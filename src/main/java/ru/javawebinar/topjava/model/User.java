11be0c452529bf36f2aa8ad930cde345c1b152a2
Esc777 <Uni_Symieb@mail.ru>
2017-05-11 19:07:27 +0800

    prepare HW5

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -117,7 +117,7 @@ public class User extends NamedEntity {
     @Override
     public String toString() {
         return "User{" +
-                "id=" + id +
+                "id=" + getId() +
                 ", email=" + email +
                 ", name=" + name +
                 ", enabled=" + enabled +
