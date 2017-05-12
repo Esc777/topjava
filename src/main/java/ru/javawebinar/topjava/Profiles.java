33f3735ea6fa719a7f67826da00315f9d49e0def
Esc777 <Uni_Symieb@mail.ru>
2017-05-12 17:28:49 +0800

    HW5.2

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -1,9 +1,5 @@
 package ru.javawebinar.topjava;
 
-/**
- * User: gkislin
- * Date: 19.08.2014
- */
 public class Profiles {
     public static final String
             POSTGRES_DB = "postgres",
@@ -12,8 +8,11 @@ public class Profiles {
     public static final String ACTIVE_DB = POSTGRES_DB;
 
     public static final String
+
             JDBC = "jdbc",
-            JPA = "jpa";
+            JPA = "jpa",
+            DATAJPA = "datajpa";
 
     public static final String REPOSITORY_IMPLEMENTATION = JPA;
+
 }
